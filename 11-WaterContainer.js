02a96994e8fbe440cfcb393df20223e0c920b9ad
Chhaian Pin <chhaianpin@hotmail.com>
2020-01-04 16:26:02 -0800

    practing leetcode problems in javascript

---
+++ 11-WaterContainer.js
@@ -0,0 +1,23 @@
+var maxArea = function(height) {
+    let maxArea = 0, leftIndex = 0, rightIndex = height.length - 1;
+
+    let count = 1;
+    // for (let i = 0; i < height.length - 1; i++) {
+    //     for (let j = i + 1; j < height.length; j++) {
+    //         let minHeight = Math.min(height[i], height[j]);
+    //         let area = minHeight * (j - i);
+    //         maxArea = Math.max(area, maxArea);
+    //     }
+    // }
+
+    while(leftIndex < rightIndex) {
+        let area = (rightIndex - leftIndex) * Math.min(height[leftIndex], height[rightIndex]);
+        maxArea = Math.max(area, maxArea);
+        height[leftIndex] <= height[rightIndex] ? leftIndex++ : rightIndex--;
+    }
+
+    return maxArea;
+}
+
+console.log(maxArea([1,8,6,2,5,5,8,3,7]));
+
