2cbffe3bb067bfd1c4ce167cd6a891d6d869774a
Chhaian Pin <chhaianpin@hotmail.com>
2020-01-10 17:39:51 -0800

    completed #22 Generate Parentheses with Breadth First Search

---
+++ 22-GenerateParentheses.js
@@ -0,0 +1,23 @@
+const generateParenthesis = (n) => {
+    const result = [];
+    breathFirstSearch("", 0, 0);
+    return result;    
+
+    function breathFirstSearch(str, left, right) {
+        if (left === n && right === n) {
+            result.push(str);
+            return;
+        }
+
+        if (left !== n) {
+            breathFirstSearch(str + "(", left + 1, right);
+        }
+
+        if (left > right) {
+            breathFirstSearch(str + ")", left, right + 1);
+        }
+    }
+}
+
+console.log(generateParenthesis(3));
+console.log(generateParenthesis(4));
